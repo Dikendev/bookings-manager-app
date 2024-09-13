c7d8e4607a26b0e04b532b908828f02af154266c
Dikendev <diken.dev@gmail.com>
2024-09-12 23:07:42 -0300

    Refactor Header component to adhere to coding style guidelines

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,23 @@
 export const Header = () => {
-    return (
-        <div>
-                <header>
-                    <nav>
-                        <ul>
-                            <li>
-                                <a href="1"></a>
-                            </li>
-                            <li>
-                                <a href="2"></a>
-                            </li>
-                            <li>
-                                <a href="3"></a>
-                            </li>
-                        </ul>
-                    </nav>
-                </header>
-            </div>
-    )
-}
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="1"></a>
+            </li>
+            <li>
+              <a href="2"></a>
+            </li>
+            <li>
+              <a href="3"></a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+};
 
-Header.displayName = 'Header'
+Header.displayName = "Header";
