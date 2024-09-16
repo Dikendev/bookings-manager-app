1a69685e895d07cd442ea57668a5bf3b1eccdf4c
Dikendev <diken.dev@gmail.com>
2024-09-15 23:00:06 -0300

    Refactor Header component to update navigation links and add icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,37 @@
+import { FaCalendarAlt, FaDoorOpen, FaUsers } from "react-icons/fa";
+
 export const Header = () => {
   return (
-    <div>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <a href="1"></a>
-            </li>
-            <li>
-              <a href="2"></a>
-            </li>
-            <li>
-              <a href="3"></a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </div>
+    <header>
+      <nav>
+        <ul className="header list">
+          <li>
+            <a href="/bookings" className="header button icon">
+              <button className="header_button">
+                <FaCalendarAlt />
+                <span>bookings</span>
+              </button>
+            </a>
+          </li>
+          <li>
+            <a href="/bookable" className="header button">
+              <button className="header_button">
+                <FaDoorOpen />
+                <span>bookables</span>
+              </button>
+            </a>
+          </li>
+          <li>
+            <a href="/user" className="header button">
+              <button className="header_button">
+                <FaUsers />
+                <span>users</span>
+              </button>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
